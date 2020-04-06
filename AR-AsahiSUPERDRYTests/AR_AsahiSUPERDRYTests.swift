977eccd4d71fb6a0311b515dcb899651908e98cd
woxjro <n0i6c0k2@gmail.com>
2020-04-06 16:46:59 +0900

    first commit

---
+++ AR-AsahiSUPERDRYTests/AR_AsahiSUPERDRYTests.swift
@@ -0,0 +1,34 @@
+//
+//  AR_AsahiSUPERDRYTests.swift
+//  AR-AsahiSUPERDRYTests
+//
+//  Created by YU on 2020/04/06.
+//  Copyright © 2020 YU. All rights reserved.
+//
+
+import XCTest
+@testable import AR_AsahiSUPERDRY
+
+class AR_AsahiSUPERDRYTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
